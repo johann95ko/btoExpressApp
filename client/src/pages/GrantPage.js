4c4cfe9185585577994dd058ae95bda4a1cec07b
Glendon Thaiw <glendon888@gmail.com>
2020-02-19 00:08:19 +0800

    Added Location Search to Trigger LatLng Coordinates Functionality

--- client/src/pages/GrantPage.js
+++ client/src/pages/GrantPage.js
@@ -1,10 +1,62 @@
+
+// import React from "react";
+// import "./pages.css";
 import React from "react";
-import "./pages.css";
+import PlacesAutoComplete, {
+    geocodeByAddress,
+    getLatLng
+
+} from "react-places-autocomplete";
+
 
 export const GrantPage = () => {
+  const [address, setAddress] = React.useState("");
+  const [coordinates, setCoordinates] = React.useState({
+    lat: 5,
+    lng: null
+  });
+
+  const handleSelect = async value => {
+    const results = await geocodeByAddress(value);
+    const latLng = await getLatLng(results[0]);
+    setAddress(value);
+    setCoordinates(latLng);
+    
+  };
+
   return (
     <div className="pageContainer">
-      <p>Grants here</p>
+      <PlacesAutoComplete
+      value={address}
+      onChange={setAddress}
+      onSelect={handleSelect}
+      >
+    
+      {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+      <div>
+            <p>Latitude: {coordinates.lat}</p>
+            <p>Longitude: {coordinates.lng}</p>
+            
+        <input {...getInputProps({ placeholder: "Type address" })} />
+
+        <div>
+          {loading ? <div>...loading</div> : null}
+
+          {suggestions.map(suggestion => {
+                const style = {
+                backgroundColor: suggestion.active ? "#41b6e7" : "#ffffff"
+                };
+
+            return (
+            <div {...getSuggestionItemProps(suggestion, { style } )}>
+            {suggestion.description}
+            </div>
+          );
+          })}
+        </div>
+      </div>
+      )}
+        </PlacesAutoComplete>
     </div>
   );
-};
+}
--- client/src/pages/LocationPage.js
+++ client/src/pages/LocationPage.js
@@ -2,10 +2,30 @@ import React, {useState} from "react";
 import "./pages.css";
 import {GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow } from "react-google-maps";
 import * as btoData from "../data/btoData.json";
+import PlacesAutoComplete, {
+  geocodeByAddress,
+  getLatLng
+
+} from "react-places-autocomplete";
 
 function Map(){
   const[selectedBto , setSelectedBto] = useState(null);
+  const [address, setAddress] = React.useState("");
+  const [coordinates, setCoordinates] = React.useState({
+    lat: null,
+    lng: null
+  });
+
+  const handleSelect = async value => {
+    const results = await geocodeByAddress(value);
+    const latLng = await getLatLng(results[0]);
+    setAddress(value);
+    setCoordinates(latLng);
+    const x = coordinates.lat;
+    const y = coordinates.lng;
+  };
   return (
+    <div>
     <GoogleMap
     defaultZoom={12}
     defaultCenter={{ lat:1.352083, lng:103.819839 }}
@@ -44,20 +64,49 @@ function Map(){
             </InfoWindow>
       )}
     </GoogleMap>
+
+{/* Search Function */}
+<PlacesAutoComplete
+value={address}
+onChange={setAddress}
+onSelect={handleSelect}
+>
+
+{({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+<div>
+  <p> ----The location details of our search are as follows ----  </p> 
+      <p>Latitude: {coordinates.lat}</p>
+      <p>Longitude: {coordinates.lng}</p>
+  <p> ----We will use this information to calculate travel durations----  </p>    
+      <Marker position={{ 
+          lat: coordinates.lat,
+          lng: coordinates.lng
+        }}
+        /> 
+      
+  <input {...getInputProps({ placeholder: "Type address" })} />
+
+  <div>
+    {loading ? <div>...loading</div> : null}
+
+    {suggestions.map(suggestion => {
+          const style = {
+          backgroundColor: suggestion.active ? "#41b6e7" : "#ffffff"
+          };
+
+      return (
+      <div {...getSuggestionItemProps(suggestion, { style } )}>
+      {suggestion.description}
+      </div>
+    );
+    })}
+  </div>
+</div>
+)}
+  </PlacesAutoComplete>
+</div>
   );
 }
-
-
-
-// function Map(){
-//   return (
-//     <GoogleMap
-//     defaultZoom={10}
-//     defaultCenter={{ lat:45.421, lng:-75.697 }}
-//     >
-// </GoogleMap>
-//   );
-// }
 
 const WrappedMap = withScriptjs(withGoogleMap(Map));
 
