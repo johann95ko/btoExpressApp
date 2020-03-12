f68ba67616031697f5646a9959d39bcf11f69eb9
Larry <larrylee3107@gmail.com>
2020-03-13 01:07:51 +0800

    user backend fix

--- src/app.js
+++ src/app.js
@@ -30,9 +30,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-
 app.use("/home", indexRouter);
-app.use("/users", usersRouter);
 app.use("/api/btoDescription", btoDescRouter);
 app.use("/api/housing", housingRouter);
 app.use("/api/grants", grantRouter);
