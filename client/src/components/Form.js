4ec1ba5b1094f02dd11f281ed25f9848306d5b44
Glendon Thaiw <glendon888@gmail.com>
2020-03-13 23:49:10 +0800

    Modified Map

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -5,11 +5,12 @@ import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Form, Button} from 'react-bootstrap'
+import  StaticMap  from './StaticMap'
 
 class AddressForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { value: '' };
+        this.state = { value: 'Singapore' };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -30,19 +31,18 @@ class AddressForm extends React.Component {
 
     render() {
         return (
-            <div style={{ height: `300px`,width:`1300px` }} >
+            <div style={{ height: `100px`,width:`1300px` }} >
+             {/* <StaticMap/> */}
             <form onSubmit={this.handleSubmit}>
-                <label>
-                    Enter your desired location: 
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                </label>
-
+                <label>Enter your desired location: <input type="text" value={this.state.value} onChange={this.handleChange}/></label>
                 <Button variant="secondary" type="submit" value="submit" >
                 Enter
                </Button>
+              
         </form>
-</div>
+        <StaticMap/>
 
+</div>
 
         );
     }
--- client/src/components/MapWithADirectionsRenderer.js
+++ client/src/components/MapWithADirectionsRenderer.js
@@ -1,24 +1,44 @@
 /*global google*/
 import React, { Component } from 'react';
-import { compose, withProps, lifecycle } from "recompose";
+
+import { compose, withProps, lifecycle} from "recompose";
+// Only can incorporate local states in class based components
+// Recompose allows the incorporation of local states in functional components
+
 import { withScriptjs,
     withGoogleMap,
     GoogleMap,
     Marker,
     InfoWindow,
     DirectionsRenderer } from 'react-google-maps';
+
 import * as btoData from "../data/btoData.json";
+import { useFetch } from "../customHooks/useFetch"
+
+import {NavBar} from "./NavBar"
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+import { BTOTableColumn } from "../components/BTOTableColumn";
 
 
 var locationNames=[]
 var durationDetails=[]
 var distanceDetails=[]
+var mainlist=[]
+
+
 const MapWithADirectionsRenderer = compose(
   
     withProps({
         googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyA_WObUiYD7YpoYufR84re1LZHAJeAGXkY",
         loadingElement: <div style={{ height: `100%` }} />,
-        containerElement: <div style={{ height: `800px`,width:`100%` }} />,
+        containerElement: <div style={{ height: `517px` }} />,
         mapElement: <div style={{ height: `100%` }} />,
     }),
     withScriptjs,
@@ -39,8 +59,6 @@ const MapWithADirectionsRenderer = compose(
                     directions2: null,
                     directions3: null,
                     newDirections: []
-                    
-
                 });
                 this.drawRoutes();
             }
@@ -51,7 +69,15 @@ const MapWithADirectionsRenderer = compose(
 
         drawRoutes() {
             // let destinations = ['Dover,Singapore', 'Jalan Membina, Singapore', 'Tampines,Singapore', 'Pasir Ris, Singapore']
-            let destinations = ['Jalan Membina,Singapore', 'Geylang Bahru, Singapore', 'Seletar,Singapore', 'Punggol, Singapore','Tampines, Singapore','Nanyang Technological University, Singapore']
+            let destinations = ['Bukit Batok, Singapore',
+                                'Choa Chu Kang, Singapore', 
+                                'Tengah, Singapore', 
+                                'Pasir Ris, Singapore',
+                                'Tampines, Singapore',
+                                'Woodlands, Singapore',
+                                'Ang Mo Kio, Singapore',
+                                'Bishan, Singapore',
+                                'Geylang, Singapore']
 
             const DirectionsService = new google.maps.DirectionsService();
             DirectionsService.route({
@@ -145,7 +171,6 @@ const MapWithADirectionsRenderer = compose(
               }
           });
 
-
           DirectionsService.route({
             origin: this.props.origin,
             destination: destinations[5],
@@ -163,13 +188,99 @@ const MapWithADirectionsRenderer = compose(
                 console.error(`error fetching directions ${result}`);
             }
         });
+
+
+        DirectionsService.route({
+            origin: this.props.origin,
+            destination: destinations[6],
+            travelMode: google.maps.TravelMode.DRIVING,
+        }, (result, status) => {
+            if (status === google.maps.DirectionsStatus.OK) {
+              locationNames.push(result.request.destination.query)
+              distanceDetails.push(result.routes[0].legs[0].distance.text)
+              durationDetails.push(result.routes[0].legs[0].duration.text)
+                this.setState({
+                    directions3: result,
+                    newDirections: this.state.newDirections.concat([result])
+                });
+            } else {
+                console.error(`error fetching directions ${result}`);
+            }
+        });
+
+
+        DirectionsService.route({
+            origin: this.props.origin,
+            destination: destinations[7],
+            travelMode: google.maps.TravelMode.DRIVING,
+        }, (result, status) => {
+            if (status === google.maps.DirectionsStatus.OK) {
+              locationNames.push(result.request.destination.query)
+              distanceDetails.push(result.routes[0].legs[0].distance.text)
+              durationDetails.push(result.routes[0].legs[0].duration.text)
+                this.setState({
+                    directions3: result,
+                    newDirections: this.state.newDirections.concat([result])
+                });
+            } else {
+                console.error(`error fetching directions ${result}`);
+            }
+        });
+
+        DirectionsService.route({
+            origin: this.props.origin,
+            destination: destinations[8],
+            travelMode: google.maps.TravelMode.DRIVING,
+        }, (result, status) => {
+            if (status === google.maps.DirectionsStatus.OK) {
+              locationNames.push(result.request.destination.query)
+              distanceDetails.push(result.routes[0].legs[0].distance.text)
+              durationDetails.push(result.routes[0].legs[0].duration.text)
+                this.setState({
+                    directions3: result,
+                    newDirections: this.state.newDirections.concat([result])
+                });
+            } else {
+                console.error(`error fetching directions ${result}`);
+            }
+        });
+
+
+        DirectionsService.route({
+            origin: this.props.origin,
+            destination: destinations[9],
+            travelMode: google.maps.TravelMode.DRIVING,
+        }, (result, status) => {
+            if (status === google.maps.DirectionsStatus.OK) {
+              locationNames.push(result.request.destination.query)
+              distanceDetails.push(result.routes[0].legs[0].distance.text)
+              durationDetails.push(result.routes[0].legs[0].duration.text)
+                this.setState({
+                    directions3: result,
+                    newDirections: this.state.newDirections.concat([result])
+                });
+            } else {
+                console.error(`error fetching directions ${result}`);
+            }
+        });
         }
     })
 )(props =>
+    <div style={{ width: "500px", height: "100px" }}>
+
+    
     <GoogleMap
         defaultZoom={7}
-        defaultCenter={new google.maps.LatLng(1.32083,102.819839)}
-        >
+        defaultCenter={new google.maps.LatLng(1.32083,102.819839)}>
+       <DirectionsRenderer directions={props.newDirections[0]} />
+       <DirectionsRenderer directions={props.newDirections[1]} />
+       <DirectionsRenderer directions={props.newDirections[2]} />
+       <DirectionsRenderer directions={props.newDirections[3]} />
+       <DirectionsRenderer directions={props.newDirections[4]} />
+       <DirectionsRenderer directions={props.newDirections[5]} />
+       <DirectionsRenderer directions={props.newDirections[6]} />
+       <DirectionsRenderer directions={props.newDirections[7]} />
+       <DirectionsRenderer directions={props.newDirections[8]} />
 
            {btoData.features.map(bto=>( 
           <Marker 
@@ -179,16 +290,9 @@ const MapWithADirectionsRenderer = compose(
             lng: bto.geometry.coordinates[0]
           }}
 
-        //   onClick={()=> {
-        //     MarkerOn=false;
-        //   }
-        // }
-
-
-          draggable = {true}
           icon={{
             url:'/home.svg',
-            scaledSize: new window.google.maps.Size(40,40)
+            scaledSize: new window.google.maps.Size(30,30)
           }}
           />  
         ))}
@@ -211,23 +315,94 @@ const MapWithADirectionsRenderer = compose(
               </div>
               </InfoWindow>
         )} */}
-      
-       <DirectionsRenderer directions={props.newDirections[0]} />
-       <DirectionsRenderer directions={props.newDirections[1]} />
-       <DirectionsRenderer directions={props.newDirections[2]} />
-       <DirectionsRenderer directions={props.newDirections[3]} />
-       <DirectionsRenderer directions={props.newDirections[4]} />
-       <DirectionsRenderer directions={props.newDirections[5]} />
-      
-      
-    <p> BTO 1: {locationNames[0]} : {distanceDetails[0]} : {durationDetails[0]} </p>
+    {/* <p> BTO 1: {locationNames[0]} : {distanceDetails[0]} : {durationDetails[0]} </p>
     <p> BTO 2: {locationNames[1]} : {distanceDetails[1]} : {durationDetails[1]}</p>
     <p> BTO 3: {locationNames[2]} : {distanceDetails[2]} : {durationDetails[2]}</p>
     <p> BTO 4: {locationNames[3]} : {distanceDetails[3]} : {durationDetails[3]}</p>
     <p> BTO 5: {locationNames[4]} : {distanceDetails[4]} : {durationDetails[4]}</p>
-    <p> BTO 6: {locationNames[5]} : {distanceDetails[5]} : {durationDetails[5]}</p>
-
+    <p> BTO 6: {locationNames[5]} : {distanceDetails[5]} : {durationDetails[5]}</p> */}
     </GoogleMap>
+
+        <TableContainer style={{ width: 800 }} component={Paper}>
+      <Table style={{ width: 800 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">Houses</TableCell>
+            <TableCell align="center">Distance</TableCell>
+            <TableCell align="center">Duration</TableCell>
+
+          </TableRow>
+        </TableHead>
+    
+        <TableBody>
+          <TableCell align="center" component="th" scope="row">{locationNames[0]}</TableCell>
+          <TableCell align="center" component="th" scope="row">{(distanceDetails.sort())[0]}</TableCell>
+          <TableCell align="center" component="th" scope="row">{(durationDetails.sort())[0]}</TableCell>
+        </TableBody>
+
+        <TableBody>
+          <TableCell align="center" component="th" scope="row">{locationNames[1]}</TableCell>
+          <TableCell align="center" component="th" scope="row">{(distanceDetails.sort())[1]}</TableCell>
+          <TableCell align="center" component="th" scope="row">{(durationDetails.sort())[1]}</TableCell>
+        </TableBody>
+    
+        <TableBody>
+          <TableCell align="center" component="th" scope="row">{locationNames[2]}</TableCell>
+          <TableCell align="center" component="th" scope="row">{(distanceDetails.sort())[2]}</TableCell>
+          <TableCell align="center" component="th" scope="row">{(durationDetails.sort())[2]}</TableCell>
+        </TableBody>
+
+            <TableBody>
+          <TableCell align="center" component="th" scope="row">{locationNames[3]}</TableCell>
+          <TableCell align="center" component="th" scope="row">{(distanceDetails.sort())[3]}</TableCell>
+          <TableCell align="center" component="th" scope="row">{(durationDetails.sort())[3]}</TableCell>
+        </TableBody>
+
+            <TableBody>
+          <TableCell align="center" component="th" scope="row">{locationNames[4]}</TableCell>
+          <TableCell align="center" component="th" scope="row">{(distanceDetails.sort())[4]}</TableCell>
+          <TableCell align="center" component="th" scope="row">{(durationDetails.sort())[4]}</TableCell>
+        </TableBody>
+
+            <TableBody>
+          <TableCell align="center" component="th" scope="row">{locationNames[5]}</TableCell>
+          <TableCell align="center" component="th" scope="row">{(distanceDetails.sort())[5]}</TableCell>
+          <TableCell align="center" component="th" scope="row">{(durationDetails.sort())[5]}</TableCell>
+        </TableBody>
+
+
+        <TableBody>
+        <TableCell align="center" component="th" scope="row">{locationNames[6]}</TableCell>
+        <TableCell align="center" component="th" scope="row">{(distanceDetails.sort())[6]}</TableCell>
+        <TableCell align="center" component="th" scope="row">{(durationDetails.sort())[6]}</TableCell>
+        </TableBody>
+
+        <TableBody>
+        <TableCell align="center" component="th" scope="row">{locationNames[7]}</TableCell>
+        <TableCell align="center" component="th" scope="row">{(distanceDetails.sort())[7]}</TableCell>
+        <TableCell align="center" component="th" scope="row">{(durationDetails.sort())[7]}</TableCell>
+        </TableBody>
+
+        <TableBody>
+        <TableCell align="center" component="th" scope="row">{locationNames[8]}</TableCell>
+        <TableCell align="center" component="th" scope="row">{(distanceDetails.sort())[8]}</TableCell>
+        <TableCell align="center" component="th" scope="row">{(durationDetails.sort())[8]}</TableCell>
+        </TableBody>
+
+        <TableBody>
+        <TableCell align="center" component="th" scope="row">{locationNames[9]}</TableCell>
+        <TableCell align="center" component="th" scope="row">{(distanceDetails.sort())[9]}</TableCell>
+        <TableCell align="center" component="th" scope="row">{(durationDetails.sort())[9]}</TableCell>
+        </TableBody>
+        </Table>
+      
+   </TableContainer>
+
+    </div>
+
+    
 );
 
 export default MapWithADirectionsRenderer;
+
+
