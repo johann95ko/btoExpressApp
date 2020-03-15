071838d203298bbaa831271314a354de7daedf2f
Jovan Chan <JOVA0001@e.ntu.edu.sg>
2020-03-16 02:16:35 +0800

    added bootstrap styling to calc

--- client/src/components/GrantCalculator.js
+++ client/src/components/GrantCalculator.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {Button, Form} from 'react-bootstrap';
 import axios from "axios";
 
@@ -50,10 +50,7 @@ class GrantCalculator extends React.Component {
   
     handleSubmit(event) {
     event.preventDefault();
-    // console.log(this.state.incomeLevel)
-    // console.log(typeof(this.state.incomeLevel))
-    console.log(this.state.FTA)
-    // console.log(this.state)
+    console.log(this.state)
 
     axios.post('http://localhost:5000/api/grants/bto', {
         incomeLevel: this.state.incomeLevel,
@@ -62,8 +59,7 @@ class GrantCalculator extends React.Component {
         employmentStatus: this.state.employmentStatus,
         })
         .then((response) => {
-            console.log(response);
-            console.log(this.state)
+            // console.log(response);
             this.setState({grant: response.data})
         })
         .catch((error) => {
@@ -74,65 +70,74 @@ class GrantCalculator extends React.Component {
     render() {
     return (
         <div>
-            <form onSubmit={this.handleSubmit}>
-                <div>
-                    <label>Are you a first time applicant?</label>
-                    <select onChange={this.handleFTA}>
-                        <option value='true'>Yes</option>
-                        <option value='false'>No</option>
-                    </select>
-                </div>
 
-                <div>
-                    <label>Is your spouse a first time applicant?</label>
-                    <select onChange={this.handleSpouseFTA}>
-                        <option value='true'>Yes</option>
-                        <option value='false'>No</option>
-                    </select>
-                </div>
+        <Form onSubmit={this.handleSubmit}>
+            <Form.Group controlId="formFTA">
+                <Form.Label>Are you a first time applicant?</Form.Label>
+                <Form.Control as="select" onChange={this.handleFTA}>
+                    <option value='true'>Yes</option>
+                    <option value='false'>No</option>
+                </Form.Control>
+            </Form.Group>
 
-                <div>
-                    <label>Combined Monthly Income for the past 12 months:</label>
-                    <select onChange={this.handleIncome}>
-                        <option value='1500'>$1,500 or below</option>
-                        <option value='2000'>$1,501 to $2,000</option>
-                        <option value='2500'>$2,001 to $2,500</option>
-                        <option value='3000'>$2,501 to $3,000</option>
-                        <option value='3500'>$3,001 to $3,500</option>
-                        <option value='4000'>$3,501 to $4,000</option>
-                        <option value='4500'>$4,001 to $4,500</option>
-                        <option value='5000'>$4,501 to $5,000</option>
-                        <option value='5500'>$5,001 to $5,500</option>
-                        <option value='6000'>$5,501 to $6,000</option>
-                        <option value='6500'>$6,001 to $6,500</option>
-                        <option value='7000'>$6,501 to $7,000</option>
-                        <option value='7500'>$7,001 to $7,500</option>
-                        <option value='8000'>$7,501 to $8,000</option>
-                        <option value='8500'>$8,001 to $8,500</option>
-                        <option value='9000'>$8,501 to $9,000</option>
-                        <option value='9001'>$9,001 and above</option>
-                    </select>
-                </div>
+            <Form.Group controlId="formSpouseFTA">
+                <Form.Label>Is your spouse a first time applicant?</Form.Label>
+                <Form.Control as="select" onChange={this.handleSpouseFTA}>
+                    <option value='true'>Yes</option>
+                    <option value='false'>No</option>
+                </Form.Control>
+            </Form.Group>
 
-                <div>
-                    <label>Employment status</label>
-                    <select onChange={this.handleEmployed}>
-                        <option value='employed'>Employed</option>
-                        <option value='unemployed'>Unemployed</option>
-                    </select>                    
-                </div>
+            <Form.Group controlId="formIncome">
+                <Form.Label>Combined Monthly Income for the past 12 months:</Form.Label>
+                <Form.Control as="select" onChange={this.handleIncome}>
+                    <option value='1500'>$1,500 or below</option>
+                    <option value='2000'>$1,501 to $2,000</option>
+                    <option value='2500'>$2,001 to $2,500</option>
+                    <option value='3000'>$2,501 to $3,000</option>
+                    <option value='3500'>$3,001 to $3,500</option>
+                    <option value='4000'>$3,501 to $4,000</option>
+                    <option value='4500'>$4,001 to $4,500</option>
+                    <option value='5000'>$4,501 to $5,000</option>
+                    <option value='5500'>$5,001 to $5,500</option>
+                    <option value='6000'>$5,501 to $6,000</option>
+                    <option value='6500'>$6,001 to $6,500</option>
+                    <option value='7000'>$6,501 to $7,000</option>
+                    <option value='7500'>$7,001 to $7,500</option>
+                    <option value='8000'>$7,501 to $8,000</option>
+                    <option value='8500'>$8,001 to $8,500</option>
+                    <option value='9000'>$8,501 to $9,000</option>
+                    <option value='9001'>$9,001 and above</option>
+                </Form.Control>
+            </Form.Group>
 
-                <input type="submit" value="Submit" />
-            </form>
-            
+            <Form.Group controlId="formEmployment">
+                <Form.Label>Employment status</Form.Label>
+                <Form.Control as="select" onChange={this.handleEmployed}>
+                    <option value='employed'>Employed</option>
+                    <option value='unemployed'>Unemployed</option>
+                </Form.Control>
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+            Submit
+            </Button>
+        </Form>
+
             <div>
-                grant: {this.state.grant}
+                Grant: {this.state.grant}
             </div>
+
         </div>
+
+
     );
 
     }
 }
+
+
+
 
 
 // const GrantCalculator = () => {
