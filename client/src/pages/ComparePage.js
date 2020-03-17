b3db9a5317e520413768fc9761c48f559bd42a11
cy002 <42965395+cy002@users.noreply.github.com>
2020-03-18 04:50:21 +0800

    Change color

--- client/src/pages/ComparePage.js
+++ client/src/pages/ComparePage.js
@@ -19,7 +19,7 @@ export const ComparePage = () => {
 
   return (
     <body>
-    <div className="main" style={{padding:"100px", paddingBottom:"30px", backgroundColor:"#DDDDEA", textAlign:"center"}}>
+    <div className="main" style={{padding:"100px", paddingBottom:"30px", backgroundColor:"#DEE4EC", textAlign:"center"}}>
       <h1>Compare BTOs</h1>
       <h6>Compare the different features of various BTOs</h6>
     </div>
