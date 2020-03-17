f5d1f838f3e2aa2c3dea7584a06b3bdb020a47cc
Larry <larrylee3107@gmail.com>
2020-03-18 01:31:52 +0800

    state lifitng to location page

--- client/src/pages/LocationPage.js
+++ client/src/pages/LocationPage.js
@@ -1,9 +1,21 @@
-import React, { Component } from "react";
-
+import React, { useEffect, useState } from "react";
 import StaticMap from "../components/StaticMap";
 import { MapsForm } from "../components/MapsForm";
+import ReactDOM from "react-dom";
+import MapWithADirectionsRenderer from "../components/MapWithADirectionsRenderer";
 
 export const LocationPage = () => {
+  const [formValue, setFormValue] = useState("");
+  const handleChange = event => {
+    setFormValue(event.target.value);
+  };
+  const handleSubmit = event => {
+    ReactDOM.render(
+      <MapWithADirectionsRenderer origin={formValue} />,
+      document.getElementById("mapplace")
+    );
+    event.preventDefault();
+  };
   return (
     <div>
       <h1>Spacer</h1>
@@ -11,7 +23,11 @@ export const LocationPage = () => {
 
       <h1>Spacer</h1>
 
-      <MapsForm />
+      <MapsForm
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+        formValue={formValue}
+      />
       <StaticMap />
     </div>
   );
