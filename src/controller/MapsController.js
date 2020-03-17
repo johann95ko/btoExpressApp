d1ea8c4f52e6edada6331e4bd6fcace658fd7294
Larry <larrylee3107@gmail.com>
2020-03-18 04:16:10 +0800

    distance api post adjustments

--- src/controller/MapsController.js
+++ src/controller/MapsController.js
@@ -1,39 +1,61 @@
 var express = require("express");
 var router = express.Router();
-const GoogleMaps = require("../models/Maps");
+const Maps = require("../models/Maps");
+
+let btoLocations = [
+  "Choa Chu Kang Grove, Keat Hong Link",
+  "Bukit Batok Road, Tegah Park Avenue",
+  "Pasir Ris Drive, Pasir Ris Green",
+  "Tampines Street 96, Tampines Street 92"
+];
 
 /* GET users listing. */
 router.route("/geocode").get((req, res) => {
-  GoogleMaps.geocode().then(result => {
+  const maps = new Maps();
+  maps.geocode().then(result => {
     res.send(result.data);
   });
 });
 
 /* ADD users listing. */
-router.route("/distance").get((req, res) => {
-  var originLoc = "Choa Chu Kang Grove, Keat Hong Link";
-  var destinationLoc = "Bukit Batok Road, Tegah Park Avenue";
-  GoogleMaps.distance(originLoc, destinationLoc).then(result => {
-    res.send(result.data);
+router.route("/distance").post((req, res) => {
+  const maps = new Maps();
+  const originLoc = req.body.originLoc;
+  const destinationLoc = req.body.destinationLoc;
+  const googleDistanceApiPromise = maps.distance(originLoc, destinationLoc);
+  googleDistanceApiPromise.then(result => {
+    res.send([result.data.routes[0].legs[0].distance.text, result.data.routes[0].legs[0].duration.text])
   });
+  // console.log(googleDistanceApi);
+  // res.send(googleDistanceApi.data.routes[0].legs[0].distance.text);
+  // console.log(mapsDistance)
+  // for (const eachLocation of btoLocations) {
+  //   distancePromises.push(maps.distance(originLoc, eachLocation));
+  //   maps.distance(originLoc, eachLocation).then(result => {
+  //     distanceArray.push(result.data.routes[0].legs[0].distance.text);
+  //   });
+  // }
+
+  // Promise.all(distancePromises).then(function(values) {
+  //   res.send(213);
+  // });
 });
 
 module.exports = router;
 
+// /* GET grants. */
+// router.route("/bto").post((req, res) => {
+//   const incomeLevel = req.body.incomeLevel;
+//   const FTA = req.body.FTA;
+//   const spouseFTA = req.body.spouseFTA;
+//   const employmentStatus = req.body.employmentStatus;
 
-/* GET grants. */
-router.route("/bto").post((req, res) => {
-  const incomeLevel = req.body.incomeLevel;
-  const FTA = req.body.FTA;
-  const spouseFTA = req.body.spouseFTA;
-  const employmentStatus = req.body.employmentStatus;
+//   const btoGrantCalculator = new BtoGrantCalculator(
+//     incomeLevel,
+//     FTA,
+//     spouseFTA,
+//     employmentStatus
+//   );
 
-  const btoGrantCalculator = new BtoGrantCalculator(
-    incomeLevel,
-    FTA,
-    spouseFTA,
-    employmentStatus
-  );
-
-  res.send(btoGrantCalculator.calulateGrant().toString());
-});
+//   res.send(btoGrantCalculator.calulateGrant().toString());
+// });
