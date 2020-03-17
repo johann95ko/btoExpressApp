d0b147d4d71d4d71cbc33dfb859c9c9776ec1cff
Larry <larrylee3107@gmail.com>
2020-03-18 01:15:49 +0800

    form and map update

--- client/src/components/MapsForm.js
+++ client/src/components/MapsForm.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
 import "./components.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import MapWithADirectionsRenderer from "./MapWithADirectionsRenderer";
 import { Navbar, Nav } from "react-bootstrap";
 import Logo from "../Images/BTOLogo.png";
 
@@ -10,13 +12,17 @@ export const MapsForm = () => {
     setFormValue(event.target.value);
   };
   const handleSubmit = event => {
+    ReactDOM.render(
+      <MapWithADirectionsRenderer origin={formValue} />,
+      document.getElementById("mapplace")
+    );
     event.preventDefault();
   };
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <label>
-          Name:
+          Enter Your Preferred Location:
           <input type="text" value={formValue} onChange={handleChange} />
         </label>
         <input type="submit" value="Submit" />
