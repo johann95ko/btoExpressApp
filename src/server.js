1e38913b58adbe5a7085c8ce4f4dfe7f12d33ae7
johann95ko <johann95ko@hotmail.com>
2020-03-19 19:22:50 +0800

    resolved server bug

--- src/server.js
+++ src/server.js
@@ -1,5 +1,20 @@
 const app = require("./app");
+var mongoose = require("mongoose");
+
+const port = process.env.port || 5000;
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`)
 });
+
+// Connecting to MongoDB via Mongoose
+mongoose
+  .connect(
+    "mongodb+srv://larrylee3107:90points!@cluster0-bpf6b.mongodb.net/test?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true
+    },
+    function(err, db) {}
+  )
+  .then(() => console.log("MongoDB Connected Successfully!")) //Display message when database is connected
+  .catch(err => console.log(err));
 
