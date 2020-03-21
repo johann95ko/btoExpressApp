48f87ab3fbaee221e961b2463f4d3f1fd93f0e26
Larry <larrylee3107@gmail.com>
2020-03-21 22:22:50 +0800

    home page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -21,33 +21,29 @@ export const HomePage = () => {
       </div>
       <div class="main" id="section2">
         <h1 className="page-title">Available BTOs</h1>
-        <h3 className="page-title-2"><center>We help you choose the right home with ease.</center></h3>
-        
-    <div class="row">
-    <div class="col-md-2">
-    </div>
+        <h3 className="page-title-2">
+          <center>We help you choose the right home with ease.</center>
+        </h3>
 
-    <div class="col-md-8">
-    <CardDeck>
-          {data.map(eachHousing => (
-            <Card
-              key={eachHousing.id}
-              btoName={eachHousing.Name}
-              shortDesc={eachHousing.ShortestDesc}
-              Image={eachHousing.Image}
-            />
-          ))}
-        </CardDeck>
-    </div>
-    <div class="col-md-2">
-    </div>
-  </div>
+        <div class="row">
+          <div class="col-md-2"></div>
 
-
-
+          <div class="col-md-8">
+            <CardDeck>
+              {data.map(eachHousing => (
+                <Card
+                  key={eachHousing.id}
+                  btoName={eachHousing.Name}
+                  shortDesc={eachHousing.ShortestDesc}
+                  Image={eachHousing.Image}
+                />
+              ))}
+            </CardDeck>
+          </div>
+          <div class="col-md-2"></div>
+        </div>
       </div>
-      <Footer/>
+      <Footer />
     </body>
   );
 };
-
