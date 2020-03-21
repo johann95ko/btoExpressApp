4bf305dc18eed948e85dfa43d7a1ca14048f8243
cy002 <42965395+cy002@users.noreply.github.com>
2020-03-21 22:07:01 +0800

    Register tab

--- client/src/components/loginSlide.js
+++ client/src/components/loginSlide.js
@@ -6,14 +6,55 @@ import Button from '@material-ui/core/Button';
 import { LoginForm } from "../components/LoginForm";
 import Image from 'react-bootstrap/Image';
 import Lari from "../Images/lariLiiiii.jpg";
+import { RegisterForm } from './RegisterForm';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
-const useStyles = makeStyles({
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
   list: {
     width: '100%',
     height: "100%"
   },
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
   
-});
+}));
 
 export const LoginDrawer = () => {
   const classes = useStyles();
@@ -24,7 +65,13 @@ export const LoginDrawer = () => {
   const [loginState, loginsetState] = React.useState({
       loggedIn: false,
   })
+  const [value, setValue] = React.useState(0);
 
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  
   const toggleDrawer = (anchor, open) => event => {
     if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
       return;
@@ -40,7 +87,6 @@ export const LoginDrawer = () => {
 
   const loggedInFunction = () =>{
     if (loginState.loggedIn === true){
-    console.log("hello this is here");
 
     return(
     <div id="lari">
@@ -51,7 +97,9 @@ export const LoginDrawer = () => {
     }
   }
 
-  const list = anchor => (
+
+  const list = anchor => {
+      return(
       <body >
     <div id="login" 
       className={clsx(classes.list)}
@@ -62,20 +110,56 @@ export const LoginDrawer = () => {
       <LoginForm/>
       <Button id="loginButton" variant="contained" color="primary" onClick={loginToggle('loggedIn', true)}>
              Log In
-         </Button>
-         {console.log(loginState)}
+      </Button>
+         
      <p>
-    <a href="/">Forgotten username or password?</a></p>
+    <a href="/">Forgot username or password?</a>
+    </p>
+    
     </div>
     </body>
-  );
+    
+      )
+  };
+
+  const fill = anchor => {
+      return(
+    <body >
+  <div id="register" 
+    className={clsx(classes.list)}
+    role="presentation"  
+  >
+      <h5>Create an Account</h5>
+  
+    <RegisterForm/>
+    <Button id="registerButton" variant="contained" color="primary" >
+           Register
+       </Button>
+  
+  </div>
+  </body>
+    
+    )
+  
+};
 
   return (
     <div>
         <React.Fragment key='right'>
           <Button id="logButton" onClick={toggleDrawer('right', true)}>Login</Button>
           <Drawer anchor='right' open={state['right']} onClose={toggleDrawer('right', false)}>
-            {list('right')}
+          <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+          <Tab label="Log In" {...a11yProps(0)} />
+          <Tab label="Register" {...a11yProps(1)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        {list('right')}
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        {fill('right')}
+      </TabPanel>
           </Drawer>
         </React.Fragment>
         {loggedInFunction()}
