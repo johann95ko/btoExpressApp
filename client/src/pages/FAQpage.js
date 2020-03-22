a3fd60b3685ab06ca39de904afe6de8f13ab9785
Larry <larrylee3107@gmail.com>
2020-03-22 16:45:21 +0800

    remove footer for faq

--- client/src/pages/FAQpage.js
+++ client/src/pages/FAQpage.js
@@ -4,52 +4,64 @@ import "./pages.css";
 import { Footer } from "../components/Footer";
 
 export const FAQpage = () => {
-  
-  return ( // printed on screen
+  return (
+    // printed on screen
     <body>
-
-        <div class="FAQbox">
+      <div class="FAQbox">
         <h1>FAQs</h1>
-          <div id="FAQaccordion">
-            <div class="card">
-              <div class="card-header" id="headingOne">
-                <h5 class="mb-0">
-                  <button type ="button" class="btn btn-link" data-toggle="collapse " data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    Question 1
-                  </button>
-                  
-                </h5>
-              </div>
-
-              <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#FAQaccordion">
-                <div class="card-body">
-                  Hello!
-                </div>
-              </div> 
+        <div id="FAQaccordion">
+          <div class="card">
+            <div class="card-header" id="headingOne">
+              <h5 class="mb-0">
+                <button
+                  type="button"
+                  class="btn btn-link"
+                  data-toggle="collapse "
+                  data-target="#collapseOne"
+                  aria-expanded="true"
+                  aria-controls="collapseOne"
+                >
+                  Question 1
+                </button>
+              </h5>
             </div>
 
-            <div class="card">
-              <div class="card-header" id="headingTwo">
-                <h5 class="mb-0">
-                  <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseTwo">
-                    Question 2
-                  </button>
-                </h5>
-              </div>
+            <div
+              id="collapseOne"
+              class="collapse show"
+              aria-labelledby="headingOne"
+              data-parent="#FAQaccordion"
+            >
+              <div class="card-body">Hello!</div>
+            </div>
+          </div>
 
-              <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent="#FAQaccordion">
-                <div class="card-body">
-                  Hello hello 2
-                </div>
-              </div>
+          <div class="card">
+            <div class="card-header" id="headingTwo">
+              <h5 class="mb-0">
+                <button
+                  class="btn btn-link"
+                  data-toggle="collapse"
+                  data-target="#collapseOne"
+                  aria-expanded="false"
+                  aria-controls="collapseTwo"
+                >
+                  Question 2
+                </button>
+              </h5>
             </div>
 
-
+            <div
+              id="collapseTwo"
+              class="collapse show"
+              aria-labelledby="headingTwo"
+              data-parent="#FAQaccordion"
+            >
+              <div class="card-body">Hello hello 2</div>
+            </div>
           </div>
         </div>
-
-    
-            <Footer/>
+      </div>
     </body>
   );
 };
