04579cc16c368ad34f6dfd177f3d8d5f0de7b599
Larry <larrylee3107@gmail.com>
2020-04-01 13:27:58 +0800

    homepage vh change

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -24,7 +24,7 @@ export const HomePage = () => {
 
   return (
     // <div className="main" id="section1">
-    <div style={{ height: "114vh" }}>
+    <div style={{ height: "118vh" }}>
       <Carousel />
     </div>
     // </div>
